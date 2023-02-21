8e7c85ce0b792bf5086b666d3c453498952e19ea
Hitodama0 <biagior00@gmail.com>
2023-02-21 16:09:18 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Biagio Ricci on 21/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
